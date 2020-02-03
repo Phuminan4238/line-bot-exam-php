875d937ccb3ff08e5064d34c5924d510a489680c
Phuminan4238 <46315222+Phuminan4238@users.noreply.github.com>
2020-02-03 20:55:18 +0700

    Update webhooks.php
    edit textMessage

--- webhooks.php
+++ webhooks.php
@@ -44,7 +44,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
-			echo json_encode($textMessage);
+			echo $textMessage;
 			curl_close($ch);
 			
 		}
